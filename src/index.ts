43622c1d3104027040186263e4f6cec34b289512
Hazim Arafa <hazimarafa80@gmail.com>
2023-02-26 20:09:51 -0800

    :wrench: Added Typescript / Nodemon Configs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import app from "./app";
 
-const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3006;
+const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3001;
 
 app.listen({ port: FASTIFY_PORT });
 
-console.log(`🚀  Fastify server running on port ${FASTIFY_PORT}`);
+console.log(`🚀 Fastify server running on port ${FASTIFY_PORT}`);
 console.log(`Route index: /`);
-console.log(`Route user: /api/v1/user`);
+console.log(`Route user: /users`);
