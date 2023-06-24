d84df85aa4738799c0add1669a07e6ba4754a225
lmaosoggypancakes <macerajosef@gmail.com>
2023-06-23 23:08:11 -0700

    🎨 basic nitro server setup

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-
-const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3001;
-
-app.listen({ port: FASTIFY_PORT });
-
-console.log(`🚀 Fastify server running on port ${FASTIFY_PORT}`);
-console.log(`Route index: /`);
-console.log(`Route user: /users`);
--- src/router.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import userController from "./controllers/users.controller";
-import indexController from "./controllers/index.controller";
-
-export default async function router(fastify: FastifyInstance) {
-  fastify.register(userController, { prefix: "/users" });
-  fastify.register(indexController, { prefix: "/" });
-}
